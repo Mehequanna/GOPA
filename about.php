115d396297938f49b7539c5f2ab707fb7f27f670
Stephen Emery <emery.stephen@gmail.com>
2017-10-08 18:16:01 -0700

    Update about and bikerack to php and use footer and navbar php files.

--- about.php
+++ about.php
@@ -25,46 +25,10 @@
   </head>
   <body>
     <!-- Start Navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html" id="icon"><img src="img/icon.png" alt="GOPA Icon" /></a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li><a href="index.html">Home <span class="sr-only">(current)</span></a></li>
-            <li class="active"><a href="about.html">About Us</a></li>
-            <li><a href="committee.html">Committee</a></li>
-            <li><a href="contact.html">Contact Us</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="nadakanaturepark.html">Nadaka Nature Park</a></li>
-                <li><a href="driscoll.html">Driscoll</a></li>
-                <li><a href="mural.html">Main Street Mural</a></li>
-                <li><a href="bikerack.html">Vintage Style Bike Rack</a></li>
-                <li><a href="crash.html">Crash</a></li>
-                <li><a href="familyties.html">Family Ties</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="whatsnewwithgopa.html">What's new with GOPA?</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="donate.html" id="donate">Donate!</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+    <?php
+      $current = 'about';
+      include('navbar.php');
+    ?>
     <!-- End Navbar -->
 
     <!-- Start Header -->
@@ -96,9 +60,7 @@
     <!-- End Body -->
 
     <!-- Footer -->
-    <div class="footer">
-      <p class="text-right">Created by Stephen Emery | Copyright 2017. All rights reserved.</p>
-    </div>
+    <?php include 'footer.php'; ?>
     <!-- End Footer -->
   </body>
 </html>
