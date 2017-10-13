b28a7ae66981a89cb3e119423d67275d1a9e7b25
Stephen Emery <emery.stephen@gmail.com>
2017-10-12 22:12:04 -0700

    Fix footer.

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-  <p class="text-right">Created by <a href="https://www.linkedin.com/in/mehequanna/" class="linkedinlink">Stephen Emery</a> | Copyright 2017. All rights reserved.</p>
+  <p class="text-right">Created by <a href="https://www.linkedin.com/in/mehequanna/" class="linkedinlink" target="_blank">Stephen Emery</a> | Copyright 2017. All rights reserved.</p>
 </div>
