7f78b29601260cfa339179c65a5cd3f53c51c9aa
Stephen Emery <emery.stephen@gmail.com>
2018-05-16 19:00:07 -0700

    Update committee members on committee.php.

--- committee.php
+++ committee.php
@@ -21,7 +21,7 @@
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
           <h2>Committee</h2>
-          <h3 class="committee text-center">Board Members</h3>
+          <h3 class="committee">Board Members</h3>
           <br>
           <h4>Judy Han, Chair</h4>
           <p>Along with her husband, Sunny, Judy has owned and successfully operated Downtown Gresham’s popular Sunny Han’s Wok & Grill eatery for twenty-six years. She has also served as President of the Downtown Gresham Boosters and seventeen years ago founded the annual Gresham Art Walk, which over the years has seen a significant increase in highly visible artist participation and visitor attendance. Judy’s contribution to the Gresham art scene has been considerable.</p>
@@ -39,32 +39,35 @@
         <div class="col-lg-10 body">
           <h3 class="committee text-center">Committee Members</h3>
           <br>
-          <h4>Rip Caswell</h4>
-          <p>Accomplished sculptor, Rip Caswell, has created more than 200 sculptures of various subjects in contemporary and realistic styles. An authority on art and sculpture, Rip often serves as a judge and juror in competitions and exhibits. He has successfully owned and operated his studio and gallery in Troutdale for over twenty years and was recently instrumental in the development and completion of the Troutdale Centennial Archway which spans the Historic Columbia River at the entrance to Historic Troutdale, one of his many outdoor public art experiences.</p>
-          <br>
-          <h4>Mike Hill</h4>
-          <p>Mike is a retired dentist and watercolor artist who still works in both areas. A member of the Gresham-Barlow School Board for nine years, Chairperson of Citizens for Schools, promoting school bond measures for over twenty years, past Board member of the Gresham Art Walk from its inception, as well as a jurist for the annual juried event, member of the National Watercolor Society, American Watercolor Society and signature member of the Northwest Watercolor Society. Mike’s artwork is in many City Hall offices as well as a mural in the City Council Chambers.</p>
-          <br>
-          <h4>Carol Nielsen</h4>
-          <p>Carol served as a Gresham City Councilor and Council liaison to the Gresham Art Committee, the Center for the Arts Foundation and Plaza and the Planning Commission. Carol’s vast community leadership roles include serving on the Mt. Hood Community College Board of Directors, member of the East Multnomah County Economic Association, Board member Gresham Historical Society and member of The Soroptomist International of Gresham.  Carol also served as the Gresham Area Chamber of Commerce Chief Executive Officer for thirteen years.</p>
-          <br>
-          <h4>Sue Myers</h4>
-          <p>Creator and Executive Director at Chase Me Again, a nonprofit thrift store, Sue Myers contributes greatly to the Gresham community. She is one of the judges and volunteer’s for the “My Mom’s a Winner” art contest, committee member for the Gresham Outdoor Public Art Project, patron of the arts and supporter for public art in Historic Downtown Gresham and support of the History Museum. Past positions held in the community: Treasurer and Director at My Father’s House, a nonprofit homeless shelter and creator and treasurer of Blue Lake Improvement Association, an environmental nonprofit.</p>
+          <h4>Joan Albertson</h4>
+          <p>Gresham resident since 1971, Joan is an active Gresham volunteer and is currently serving as a Gresham Police Department Citizen Volunteer in Policing, member of the Gresham Redevelopment Commission Advisory Committee, Chamber of Commerce Government Affairs Committee and AAUW Gresham Area Branch. She was licensed by the Oregon Board of Accountancy as a Certified Public Accountant (now retired) in 1969 when there were less than 2000 women CPAs in the United States. After leaving public accounting, she was Controller for a business forms manufacturer for several years. Prior activities have included the Gresham Charter Review Committee and Center for the Arts Foundation. </p>
           <br>
           <h4>David Baumann</h4>
-          <p>David is an art collector (impressionist paintings and figurative bronzes) and has been involved in the art industry through career and personal experiences for over thirty years. This experience has brought many long standing relationships with fine artists across the United States, and especially in the northwest region. He has been a Board member of the Gresham Arts Commission, Mt. Hood Jazz Festival and the Gresham Historical Society as well as past President of the Troutdale Chamber of Commerce.</p>
-          <br>
-          <h4>Martin Stone</h4>
-          <p>A Real Estate Broker since 1997, Marty has served the Gresham community and surrounding areas in many ways. For twenty-seven years he worked in retail sales in the family business, past President and Board member of the Gresham Downtown Boosters, East Metro Association and the Gresham Downtown Development Association. Currently he serves on the Board of the Gresham Historical Society.</p>
-          <br>
-          <h4>Gretchen Brown</h4>
-          <p>Gretchen is retired now, but formerly the Manager of the Gresham Regional Branch of Multnomah County Library from 1997 to 2011.  Currently she is a Board member of the Oregon Trail Chapter, National Society Daughters of the American Revolution, one half of Team Poppy, training and competing in Canine Scent Work trials and freelance research.</p>
-          <br>
-          <h4>Kevin Conord</h4>
-          <p>Kevin is the owner of Jazzy Bagel Company, a small business located on the corner or Powell Boulevard and Main Avenue in Historic Downtown Gresham. This bicycle friendly eatery is a popular gathering place for breakfast or lunch and has the best bagels for miles around. Some of Kevin’s past positions held in the community are: served on the City of Gresham Tree Preservation Committee and as a Board member Gresham Downtown Development Association. He has volunteered, supported and donated to numerous organizations and events in Gresham and the surrounding areas.  A patron of the arts, Kevin is gifted with a strong sense of visual creativity.</p>
+          <p>David is a private fine art collector and has been involved in the art industry through career and personal experiences for over thirty years. This experience has brought many long standing relationships with fine artists across the United States, and especially in the northwest region. He has been a Board member of the Gresham Arts Commission, Mt. Hood Jazz Festival and the Gresham Historical Society as well as past President of the Troutdale Chamber of Commerce.</p>
           <br>
           <h4>Charlotte Becker</h4>
-          <p>Charlotte was a member of the Gresham Art Walk committee for 11 years, implementing and overseeing recycling and waste management for the event. Until retiring at the end of December 2015, she served veterans and their families for 8 years as a licensed clinical social worker in Vancouver and East Multnomah County. Prior to the VA, Charlotte and her partner managed the Association of Oregon Recyclers and contracted with government agencies for public outreach materials. She was the “Voice of OPB TV” for 25 years and is currently the voice of government interactive voice response systems in many US cities.</p>
+          <p>Charlotte served on the Gresham Art Walk committee for 11 years, implementing and overseeing recycling and waste management for the event. Currently, she serves on the City’s Historic Resources Subcommittee. Charlotte and her partner, Howard Huntley, managed the Association of Oregon Recyclers and contracted with governments and private businesses on public outreach materials for 15 years, before Charlotte returned to school to get her Masters in Social Work. Prior to retiring in 2015, she worked in long-term hospice and primary care through the Portland VA Healthcare System for 8 years as a Licensed Clinical Social Worker. She was also the “Voice of OPB TV” for 25 years and is currently the voice on government Interactive Voice Response (IVR) systems in many US cities and counties.</p>
+          <br>
+          <h4>Kevin Conord</h4>
+          <p>Kevin is a retired small business owner in Historic Downtown Gresham and has held many positions in the community. Some of Kevin’s past positions are: served on the City of Gresham Tree Preservation Committee, past Board member of Gresham Downtown Development Association and past Director of the Gresham Historical Society. He volunteered, supported and donated to numerous organizations and events in Gresham and the surrounding areas. A patron of the arts, Kevin is gifted with a strong sense of visual creativity.</p>
+          <br>
+          <h4>Heather Söderberg-Greene</h4>
+          <p>Heather is an artist, sculptor, and a certified scuba diver! At an early age she gained worldwide attention as a sculpting prodigy and continued to progress as an artist hired for many public, private and museum commissions. Heather bought her own foundry  and moved it to the beautiful heart of the Columbia River Gorge where she and her husband Richard work together creating monuments and timeless bronze statues for collectors and institutions worldwide. Heather is currently sculpting the world's largest bronze bald eagle - entirely by hand.  Her Sacagawea, Pompi, and Seaman, commissioned by the Port of Cascade Locks, has gained regional and worldwide attention for its emotional impact and Heather's talent.</p>
+          <br>
+          <h4>Carol Nielsen</h4>
+          <p>Carol is a past Gresham City Councilor having served as the Council liaison to the Gresham Art Committee, the Center for the Arts Foundation and Plaza and the Planning Commission and still serves on various committees for the City and Multnomah County. Carol’s vast community leadership roles include serving on the Mt. Hood Community College Board of Directors, the East Multnomah County Economic Association, and Board member for the Gresham Historical Society and past President of Soroptomist International of Gresham. Carol recently retired from her thirteen year tenure as CEO of the Gresham Area Chamber of Commerce and Visitors Center.</p>
+          <br>
+          <h4>Sue Myers</h4>
+          <p>Creator and Executive Director at Chase Me Again, a nonprofit thrift store, Sue Myers contributes greatly to the Gresham community. She is one of the judges and volunteer’s for the “My Mom’s a Winner” art contest, Committee member for the Gresham Outdoor Public Art Project, patron of the arts and supporter for public art in Historic Downtown Gresham and support of the Gresham History Museum. Past positions held in the community: Treasurer and Director at My Father’s House, a nonprofit homeless shelter and creator and treasurer of Blue Lake Improvement Association an environmental nonprofit.</p>
+          <br>
+          <h4>Martin Stone</h4>
+          <p>A Real Estate Broker since 1997, Marty has served the Gresham community and surrounding areas in many ways. For twenty-seven years he worked in retail sales in the family business and past President and Board member of the Gresham Downtown Boosters and the Gresham Downtown Development Association. Currently he serves the Gresham Historical Society and East Metro Association as a Board member.</p>
+          <br>
+          <h4>Rip Caswell</h4>
+          <p>Accomplished sculptor, Rip Caswell, has created more than 200 sculptures of various subjects in contemporary and realistic styles. An authority on art and sculpture, Rip often serves as a judge and juror in competitions and exhibits. He has successfully owned and operated his studio and gallery in Troutdale for over twenty years and was recently instrumental in the development and completion of the Troutdale Centennial Archway which spans the Historic Columbia River at the entrance to Historic Troutdale, one of his many outdoor public art experiences. </p>
+          <br>
+          <h4>Mike Hill      1945 - 2016 In Memoriam</h4>
+          <p>Mike was a watercolor artist extraordinaire. A member of the Gresham-Barlow School Board for nine years, Chairperson of Citizens for Schools, promoting school bond measures for over twenty years, past Board member and Artistic Advisor for the Gresham Art Walk from its inception, a jurist for the annual juried event, member of the National Watercolor Society, American Watercolor Society and signature member of the Northwest Watercolor Society.  Mike’s artwork is in many City Hall offices as well as a mural in the City Council Chambers.</p>
         </div><!-- body div close -->
       </div><!-- row div close -->
     </div><!-- container div close -->
