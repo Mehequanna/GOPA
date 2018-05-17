f28e5f70a3715c9a6e7820d4d70028b994fe03c0
Stephen Emery <emery.stephen@gmail.com>
2018-05-16 19:36:34 -0700

    Update Tim's bio on bikerack.php.

--- bikerack.php
+++ bikerack.php
@@ -15,21 +15,32 @@
     <!-- End Header -->
 
     <!-- Start Body -->
-    <div class="container">
+    <div class="container text-center">
       <img src="img/divider.png" alt="Page Divider" id="divider"/>
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Vintage Style Bike Rack</h2>
+          <h2>Vintage Style Bike Rack</h2>
           <br>
           <img src="img/bikeracks/carlabikerack.png" alt="Photo of Carla Piluso with the Bike Rack" class="center"/>
-          <p class="text-center">Carla Piluso is the sponsor of the first artistic bike rack. The bike rack has been placed near Jazzy bagel on the corner of Main Ave and E Powell Blvd.</p>
+          <p>Carla Piluso is the sponsor of the first artistic bike rack. The bike rack has been placed near Jazzy bagel on the corner of Main Ave and E Powell Blvd.</p>
           <br>
-          <h2 class="text-center">Artist: Tim Foertsch</h2>
+          <h2>Artist: Tim Foertsch</h2>
           <img src="img/bikeracks/bikerack.jpg" alt="Tim Foertsch Photo" class="center"/>
-          <p class="text-center">Vintage looking bike rack with the artist Tim Foertsch.
+          <p>Tim Foertsch with his first bike rack creation.</p>
+          <p><em>“I have been reusing old, unwanted things in projects and gifts forever. I find taking something that had a former life or purpose and giving it new life inspirational.”</em>
           <br>
-          Tim Foertsch transformed a standard bike rack using reclaimed materials into a Vintage style bike rack for us.</p>
+          ~Tim Foertsch, Recycled Mixed Media artist</p>
+
+          <p>Tim Foertsch has been designing and crafting whimsical bike racks for Gresham Outdoor Public Art since 2017, after GOPA Chair, Judy Han, approached him with the idea. Each rack is made from scrap metal, and metal parts from machines and farm implements, then painted and powder coated for durability. Tim says, “I have enjoyed meeting many of the great Gresham champions and the opportunity share my work.”</p>
+
+          <p>His raw material is typically found rusting in junk piles or fields near Tim’s home in Corbett. He and his wife moved to Corbett about 15 years ago to be near family and raise their two daughters. His day job is Telecommunications Engineer. Turning scrap metal into art, as more than a hobby, came after he visited a recycled art show. It was a “light bulb moment, as I realized I could make a little extra money doing what I enjoyed.” He introduced his work to the general public at Cracked Pots Recycled Art show at McMenamins Edgefield, Troutdale, in 2009. His first gallery show was in 2012.</p>
+
+          <p>Bike racks are just a small slice of what Tim creates from the stuff many see as junk. Currently his art is available at Grizzly Ridge Upcycle, Sisters, OR; Off The Walls Gallery, Shelton, WA; and more.</p>
+
+          <p>He has participated in the Vancouver, WA Recycled Arts Festival, the Portland Yard, Garden and Patio Show, Cracked Pots at Edgefield, and several other Northwest group shows. In 2012, his work was the winner of the People’s Choice Award on the Columbia Gorge Fall Foliage Tour.</p>
+
+          <p>Tim’s website, haoleware.com, shows the range of his talents, from unique fire pits to creatures that can only live in the imagination. And for the first time in 2018, Tim has found time in his busy schedule to participate in the Annual Gresham Arts Festival, the third Saturday in July.</p>
         </div>
       </div><!-- row div close -->
     </div><!-- container div close -->
