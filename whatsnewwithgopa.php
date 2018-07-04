afabd194b2406ecf0d76fbf6f63966705fd8449f
Stephen Emery <emery.stephen@gmail.com>
2018-07-03 20:54:47 -0700

    Add new item to what's new with gopa and update index.html.

--- whatsnewwithgopa.php
+++ whatsnewwithgopa.php
@@ -15,18 +15,32 @@
     <!-- End Header -->
 
     <!-- Start Body -->
-    <div class="container">
+    <div class="container text-center">
       <img src="img/divider.png" alt="Page Divider" id="divider"/>
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Judy on Community Hotline! June 28th, 2017</h2>
+          <h2>Judy on Community Hotline! July 4th, 2018</h2>
+          <br>
+          <img src="img/communityhotlinejune28th2017.png" alt="Judy with Carla and Monica on Community Hotline" class="center"/>
+          <p>Judy Han and Carla Piluso join Monica Weitzel on Community Hotline. They talk about the Gresham Arts Festival happening on July 20th and 21st, 2018!</p>
+          <br>
+          <iframe src="https://player.vimeo.com/video/277660868" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <p>Make full screen for best viewing results.</p>
+        </div>
+      </div><!-- row div close -->
+
+      <img src="img/divider.png" alt="Page Divider" id="divider"/>
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+          <h2>Judy on Community Hotline! June 28th, 2017</h2>
           <br>
           <img src="img/communityhotlinejune28th2017.png" alt="Judy and Greg on Community Hotline" class="center"/>
-          <p class="text-center">Judy Han and Greg Matthews (Gresham Fire Chief) join Monica Weitzel on Community Hotline. They talk the Gresham Arts Festival happening on July 14th and 15th, 2017!</p>
+          <p>Judy Han and Greg Matthews (Gresham Fire Chief) join Monica Weitzel on Community Hotline. They talk about the Gresham Arts Festival happening on July 14th and 15th, 2017!</p>
           <br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/Z8jA6LpLTmI?t=1m27s" frameborder="0" allowfullscreen class="center"></iframe>
-          <p class="text-center">Make full screen for best viewing results.</p>
+          <p>Make full screen for best viewing results.</p>
         </div>
       </div><!-- row div close -->
 
@@ -34,12 +48,12 @@
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Judy and Charlotte on Community Hotline!</h2>
+          <h2>Judy and Charlotte on Community Hotline!</h2>
           <br>
-          <p class="text-center">Judy Han and Charlotte Becker join Monica Weitzel on Community Hotline. They talk about GOPA's future projects and partners!</p>
+          <p>Judy Han and Charlotte Becker join Monica Weitzel on Community Hotline. They talk about GOPA's future projects and partners!</p>
           <br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/IiJKnBu4xO8" frameborder="0" allowfullscreen class="center"></iframe>
-          <p class="text-center">Make full screen for best viewing results.</p>
+          <p>Make full screen for best viewing results.</p>
         </div>
       </div><!-- row div close -->
 
@@ -47,12 +61,12 @@
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Gresham Area Chamber of Commerce Video</h2>
+          <h2>Gresham Area Chamber of Commerce Video</h2>
           <br>
-          <p class="text-center">Driscoll (the guide dog statue), Main Street Mural, and Family Ties (GOPA provided the brick platform) were featured in the Gresham Area Chamber of Commerce video! The art is shown just after 2:35 in the video. Definitely watch the whole video to get a great look at what Gresham, Oregon has to offer.</p>
+          <p>Driscoll (the guide dog statue), Main Street Mural, and Family Ties (GOPA provided the brick platform) were featured in the Gresham Area Chamber of Commerce video! The art is shown just after 2:35 in the video. Definitely watch the whole video to get a great look at what Gresham, Oregon has to offer.</p>
           <br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/Fr723KmbcE8" frameborder="0" allowfullscreen class="center"></iframe>
-          <p class="text-center">Make full screen for best viewing results.</p>
+          <p>Make full screen for best viewing results.</p>
         </div>
       </div><!-- row div close -->
 
@@ -60,9 +74,9 @@
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Quote from Gresham Mayor, Shane Bemis</h2>
+          <h2>Quote from Gresham Mayor, Shane Bemis</h2>
           <br>
-          <p class="text-center">“I applaud the work that GOPA is doing to bring    public art to our community. Each unique piece reflects Gresham’s love of the arts while enhancing the flavor and feeling of our community.”
+          <p>“I applaud the work that GOPA is doing to bring    public art to our community. Each unique piece reflects Gresham’s love of the arts while enhancing the flavor and feeling of our community.”
             <br>
           ~ Shane Bemis</p>
         </div>
@@ -72,13 +86,13 @@
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2 class="text-center">Judy Han on Community Hotline!</h2>
+          <h2>Judy Han on Community Hotline!</h2>
           <br>
           <img src="img/judyonhotline.png" alt="Judy on Community Hotline" class="center"/>
-          <p class="text-center">Judy Han (President) and Sue Myers (Committee Member) appeared on Community Hotline to talk about the amazing work Gresham Outdoor Public Art is doing!</p>
+          <p>Judy Han (President) and Sue Myers (Committee Member) appeared on Community Hotline to talk about the amazing work Gresham Outdoor Public Art is doing!</p>
           <br>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/TcRfFtuFtBQ" frameborder="0" allowfullscreen class="center"></iframe>
-          <p class="text-center">Make full screen for best viewing results.</p>
+          <p>Make full screen for best viewing results.</p>
         </div>
       </div><!-- row div close -->
     </div><!-- container div close -->
