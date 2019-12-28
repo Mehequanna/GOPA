1c5e7f4b8937f0d6f6a612442a453127a93f9fc9
Stephen Emery <emery.stephen@gmail.com>
2019-12-27 21:56:36 -0800

    Add Imagine Page

---
+++ imagine.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include 'head.php' ?>
+  <body>
+    <!-- Start Navbar -->
+    <?php
+      $current = 'imagine';
+      include 'navbar.php';
+    ?>
+    <!-- End Navbar -->
+
+    <!-- Start Header -->
+    <div id="header">
+    </div>
+    <!-- End Header -->
+
+    <!-- Start Body -->
+    <div class="container text-center">
+      <img src="img/divider.png" alt="Page Divider" id="divider"/>
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+          <h2>Imagine, Glass Sculptures</h2>
+          <br>
+          <img src="img/imagine/glassart.jpg" alt="Glass sculptures" class="center"/>
+          <br>
+          <img src="img/imagine/glassartplaque.jpg" alt="Plaque for glass sculptures" class="center"/>
+        </div>
+      </div><!-- row div close -->
+
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+          <h2>Gresham celebrates public art <br>by Christopher Keizur for Gresham Outlook</h2>
+          <br>
+          <img src="img/imagine/glassarticle1.jpg" alt="Artist and students standing around the glass sculptures" class="center"/>
+          Outlook Photo: Christopher Keizur - Jen Fuller, middle, designed and created the new public art in downtown Gresham alongside students from Mt. Hood Community College.
+          <br>
+          <br>
+
+          <p><strong>Glass sculptures in downtown come courtesy of Mt. Hood Community College student artists</strong></p>
+
+          <p>New art is gracing downtown Gresham thanks to a collaboration between a Portland artist and 15 students from Mt. Hood Community College.</p>
+
+          <p>“Imagine" was created by artist Jen Fuller, a former educator at MHCC, and students from the college's Visual Arts department. Located at the corner of Northeast Hood Avenue and Northeast Third Street, the project was supported by the city of Gresham and Gresham Outdoor Public Art.</p>
+
+          <img src="img/imagine/glassarticle2.jpg" alt="Glass sculptures" class="center"/>
+          Outlook Photo: Christopher Keizur - The city's newest public art, Imagine, can be found at the corner of Northeast Hood Avenue and Northeast Third Street.
+          <br>
+          <br>
+
+          <p>"This was one of the nicest projects I have ever worked on," said Fuller, a glass-and-steel installation artist based out of Portland.</p>
+
+          <p>"Imagine" is the first piece of public art Fuller has installed in Gresham. She celebrated the dedication alongside her fellow artists and members of the community on Wednesday evening, May 30.</p>
+
+          <p>The art has three columns of stained glass sharing the phrase "Imagine the Possibility," with panels depicting scenes created by the students who helped put the piece together. The result is an eclectic combination of artistic visions.</p>
+
+          <p>"Each one of the students have their own style, so we looked for ways to express their individuality through the work," Fuller said.</p>
+
+          <p>The students who helped complete the piece were Chelsea Halpin, James Edwards, Shannon Clare, Colton South, Micah Whalen, Samuel Turner, Kate Vincent, Molly Garnette Anderson, Meg Turner, Emily Miller, Jade Souza, Jean-Louis Moch Van Mol, Rafael Ordorica-Rea, Julie Chura and Carl Grassl.</p>
+
+          <img src="img/imagine/glassarticle3.jpg" alt="Glass sculptures" class="center"/>
+          Outlook Photo: Christopher Keizur - Portland-based artist Jen Fuller coordinated the art, which features ideas from the MHCC students who helped put it together.
+          <br>
+          <br>
+
+          <p>Many of the MHCC artists had never worked with glass before, and loved being able to explore the new medium. They were chosen by merit to help complete the year-long process of bringing the art into downtown.</p>
+
+          <p>Because of the complications of completing the piece, and last-minute adjustments, they realized "Possibility" was misspelled on the final panel. That part of the piece will be fixed soon.</p>
+
+          <p>
+            Read the original article by Christopher Keizur at: <br>
+            <a href="https://pamplinmedia.com/go/42-news/397553-291418-gresham-celebrates-public-art" target="_blank">https://pamplinmedia.com/go/42-news/397553-291418-gresham-celebrates-public-art</a>
+          </p>
+        </div>
+      </div><!-- row div close -->
+    </div><!-- container div close -->
+    <!-- End Body -->
+
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+    <!-- End Footer -->
+  </body>
+</html>
