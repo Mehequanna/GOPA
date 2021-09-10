77a635b1b6fa45bba43d6e99da3bfd990f1a634c
Stephen Emery <emery.stephen@gmail.com>
2021-09-09 22:03:23 -0700

    Remove Fundraiser from donate page.

--- donate.php
+++ donate.php
@@ -17,27 +17,11 @@
     <!-- Start Body -->
     <div class="container text-center">
       <img src="img/divider.png" alt="Page Divider" id="divider"/>
+
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2>Todd Kirnan's Statue Fundraiser</h2>
-          <p>We are currently holding a fundraiser to commission a statue of our friend Todd. To learn more about his story, see this article from the Gresham Outlook!</p>
-          <h4><a href="https://pamplinmedia.com/go/44-features/393479-286300-mr-gresham-todd-kirnan-is-one-of-regions-extraordinary-citizens" target="blank">Click here to read Gresham Outlook's article about Todd Kirnan.</a></h4>
-          <br>
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="KRVHNN6LSGA7L">
-            <input type="image" src="img/todd_donate.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" class="center" id="donate-todd">
-            <img alt="Todd's Donation Button" border="0" src="img/todd_donate.jpg" width="1" height="1">
-          </form>
-          <br>
-          <p>If you would like to make a check out instead, please use the address below and write, "For Todd's statue" in the note section.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10 body">
-          <h2>For all other donations to Gresham Outdoor Public Art:</h2>
+          <h2>For donations to Gresham Outdoor Public Art:</h2>
           <p>The Gresham Outdoor Public Art committee is currently working on several projects. As you hear about a project you can participate with us by donating money to the fund.<br><br>
           Please mail checks to:<br>
           Gresham Outdoor Public Art<br>
