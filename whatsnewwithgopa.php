8d8e8007bf595c237135e4488cd944266e815c67
Stephen Emery <emery.stephen@gmail.com>
2022-04-19 22:04:52 -0700

    Add Hull article.

--- whatsnewwithgopa.php
+++ whatsnewwithgopa.php
@@ -59,6 +59,81 @@
       </div><!-- row div close -->
 
       <div class="row">
+          <div class="col-lg-1"></div>
+          <div class="col-lg-10 body">
+            <h2>Hull Foundation makes Gresham's public art more accessible
+              <br>by Christopher Keizur for Sandy Post</h2>
+
+              <br>
+              <p><strong>Nonprofit organization shares suggestions to improve plaques for blind community during fun outing</strong></p>
+
+              <img src="img/whatsnew/hullarticle1.jpg" alt="Two people touching Teddy the bear statue." class="center"/>
+              PMG PHOTO: CHRISTOPHER KEIZUR - The Hull Foundation and Learning Center hosted an outing to explore the bronze statues in downtown Gresham.
+              <br>
+              <br>
+
+              <p>
+                A group from a Sandy nonprofit organization dedicated to teaching skills and independence to blind community members visited downtown Gresham to not only explore and feel the tactile art, but also help make those pieces more accessible and informative to all.
+              </p>
+              <p>
+                The Hull Foundation and Learning Center, 43233 S.E. Oral Hull Road, which serves adults with blindness and sight loss, helmed an excursion into Gresham Wednesday afternoon, April 6, to visit the many bronze statues along Main Avenue. They reached up toward the top of towering "Blue" the heron; pet "Driscoll" and laughing at his fur; and sat beside "Teddy."
+              </p>
+              <p>
+                At the end of the afternoon the group enjoyed lunch at The Local Cow and bowled at Mt. Hood Lanes.
+              </p>
+              <p>
+                "This is a fun day for us to get out and explore," said KAT Rogers, program director of retreats and sight loss instructor. "We are visiting the statues in downtown and showing them how the art is tactile — that is something you don't find in many places."
+              </p>
+
+              <img src="img/whatsnew/hullarticle2.jpg" alt="Group of people walking down the side walk." class="center"/>
+              PMG PHOTO: CHRISTOPHER KEIZUR - KAT Rogers, program director of retreats, helmed the trip into Gresham.
+              <br>
+              <br>
+
+              <p>
+                This is an exciting time for the group, as they celebrate their 60th anniversary. The Hull Foundation hosts camps and retreats, fun outings, adventurous activities like skydiving and skiing, and a learning center with seminars to teach those new to being visually impaired.
+              </p>
+              <p>
+                "The Hull Foundation does these physical activities to offer independence," Rogers added. "We want to share hopes and dreams and not have people be stuck inside their house."
+              </p>
+              <p>
+                And while the group heralded the public art in Gresham as being great for blind and visually impaired visitors, there is room for improvement. So, working alongside the Gresham Outdoor Public Art Commission, the plaques at the artwork are going to be updated to be safer, easier to reach, and have more descriptive braille.
+              </p>
+
+              <img src="img/whatsnew/hullarticle3.jpg" alt="Two people reading braille sign next to Blue." class="center"/>
+              PMG PHOTO: CHRISTOPHER KEIZUR - Annie reads the braille sign beside the Blue statue — the plaques in Gresham are going to be updated to be more descriptive and accessible after suggestions from the Hull Foundation.
+              <br>
+              <br>
+
+              <p>
+                Signs with braille has been the norm in Gresham after an "aha" moment from Judy Han, who helps run nonprofit GOPA. She was driving past "Driscoll," a statue put up in 2016 to honor the community's connection to guide dogs, when she realized the sign couldn't be read by the blind.
+              </p>
+              <p>
+                "We always wanted the art in Gresham to be inclusive," Han said.
+              </p>
+              <p>
+                From that moment, all new pieces commissioned by GOPA include the information in braille, including the many murals across the city. But there were still some issues. Some of the braille on the signs is incorrect, and others don't explain what the statue is of — like "Blue" which doesn't call it a heron anywhere on the plaque. Some of the plaques are in difficult places to reach, while the sharp corners on others could be painful to a sightless person bending down to feel the piece.
+              </p>
+              <p>
+                All of those things were brought up by the Hull Foundation during its fun outing, and GOPA is committed to making the changes. The signs are being replaced to better the braille, and others are going to be repositioned for ease of access. The group is also investigating rubber ends to dull the pointy edges.
+              </p>
+              <p>
+                "We are going to make this work for everyone," Han said.
+              </p>
+
+              <p><strong>
+                Displaying art
+              </strong></p>
+              <p>
+                The Hull Foundation and Learning Center has many talented blind and visually impaired artists working across various mediums — 3D art, photography, painting and more.
+              </p>
+              <p>
+                The nonprofit group is seeking galleries or businesses interested in displaying that work. Contact the Hull Foundation at 503-668-6195 or learn more at hullparkfortheblind.org
+              </p>
+          </div>
+      </div><!-- row div close -->
+
+      <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
           <h2>Gresham Area Chamber of Commerce Video</h2>
