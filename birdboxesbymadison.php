290ac93f93bf121ea8abb4a6cb86407a40b0c926
Stephen Emery <emery.stephen@gmail.com>
2024-01-12 22:33:59 -0800

    Split bird box pages into two. One for Rico and one for Madison.

---
+++ birdboxesbymadison.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php' ?>
+<body>
+<!-- Start Navbar -->
+<?php
+$current = 'birdboxesbymadison';
+include 'navbar.php';
+?>
+<!-- End Navbar -->
+
+<!-- Start Header -->
+<div id="header">
+</div>
+<!-- End Header -->
+
+<!-- Start Body -->
+<div class="container text-center">
+    <img src="img/divider.png" alt="Page Divider" id="divider"/>
+    <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+            <h2>Bird Boxes</h2>
+            <br>
+            <img src="img/birdboxes/birdboxes1.jpg" alt="Madison spraying the base coat of white paint on an electrical box." class="center"/>
+            <br>
+            <img src="img/birdboxes/birdboxes2.jpg" alt="Madison outlining the Spotted Towhee on an electrical box." class="center"/>
+            <br>
+            <img src="img/birdboxes/birdboxes3.jpg" alt="Madison adding the finishing touches of leaves to an electrical box." class="center"/>
+            <br>
+            <img src="img/birdboxes/birdboxes4.jpg" alt="Finished electrical box with the Spotted Towhee showing two sides of the box. 
+                Left side: Spotted Towhee landing on branch. Right side: Spotted Towhee sitting on branch." class="center"/>
+            <br>
+            <p>Spotted Towhee, by Madison Hughes, located at the corner of Division and Eastman Parkway.</p>
+            <br>
+            <img src="img/birdboxes/birdboxes5.jpg" alt="Finished electrical box with the Red-Tailed Hawk on two sides. 
+                Left side: Hawk sitting on branch. Right side: Hawk flying." class="center"/>
+            <br>
+            <p>Red-Tailed Hawk, by Madison Hughes, located at the corner of Powell and Eastman Parkway.</p>
+        </div>
+    </div><!-- row div close -->
+
+    <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+            <img src="img/birdboxes/birdboxes2.jpg" alt="Madison painting an electrical box wearing a mask." />
+            <h2 class="text-center">Artist: Madison Hughes</h2>
+            <p>
+            Madison is an artist who lives in Portland, Oregon. He started painting murals and 
+            large scale work in high school and since then has painted murals all over the world. 
+            When getting to design his own murals, he often chooses to paint subject matter such 
+            as plants, animals, and other things that are significant to the location of the piece. 
+            His style can best be described as illustrative folk art inspired by contemporary design.
+            </p>
+
+        </div>
+    </div><!-- row div close -->
+</div><!-- container div close -->
+<!-- End Body -->
+
+<!-- Footer -->
+<?php include 'footer.php'; ?>
+<!-- End Footer -->
+</body>
+</html>
--- birdboxesbyrico.php
+++ birdboxesbyrico.php
@@ -4,7 +4,7 @@
 <body>
 <!-- Start Navbar -->
 <?php
-$current = 'birdboxes';
+$current = 'birdboxesbyrico';
 include 'navbar.php';
 ?>
 <!-- End Navbar -->
@@ -21,22 +21,6 @@ include 'navbar.php';
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
             <h2>Bird Boxes</h2>
-            <br>
-            <img src="img/birdboxes/birdboxes1.jpg" alt="Madison spraying the base coat of white paint on an electrical box." class="center"/>
-            <br>
-            <img src="img/birdboxes/birdboxes2.jpg" alt="Madison outlining the Spotted Towhee on an electrical box." class="center"/>
-            <br>
-            <img src="img/birdboxes/birdboxes3.jpg" alt="Madison adding the finishing touches of leaves to an electrical box." class="center"/>
-            <br>
-            <img src="img/birdboxes/birdboxes4.jpg" alt="Finished electrical box with the Spotted Towhee showing two sides of the box. 
-                Left side: Spotted Towhee landing on branch. Right side: Spotted Towhee sitting on branch." class="center"/>
-            <br>
-            <p>Spotted Towhee, by Madison Hughes, located at the corner of Division and Eastman Parkway.</p>
-            <br>
-            <img src="img/birdboxes/birdboxes5.jpg" alt="Finished electrical box with the Red-Tailed Hawk on two sides. 
-                Left side: Hawk sitting on branch. Right side: Hawk flying." class="center"/>
-            <br>
-            <p>Red-Tailed Hawk, by Madison Hughes, located at the corner of Powell and Eastman Parkway.</p>
             <br>
             <img src="img/birdboxes/birdboxes10.jpg" alt="Rico spray painting branch on electrical box." class="center"/>
             <br>
@@ -58,23 +42,6 @@ include 'navbar.php';
             <br>
             <p>By Rico Ricardo Alvarez, located at the corner of Springwater Trail and Towle.</p>
             <br>
-        </div>
-    </div><!-- row div close -->
-
-
-    <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10 body">
-            <img src="img/birdboxes/birdboxes2.jpg" alt="Madison painting an electrical box wearing a mask." />
-            <h2 class="text-center">Artist: Madison Hughes</h2>
-            <p>
-            Madison is an artist who lives in Portland, Oregon. He started painting murals and 
-            large scale work in high school and since then has painted murals all over the world. 
-            When getting to design his own murals, he often chooses to paint subject matter such 
-            as plants, animals, and other things that are significant to the location of the piece. 
-            His style can best be described as illustrative folk art inspired by contemporary design.
-            </p>
-
         </div>
     </div><!-- row div close -->
 
