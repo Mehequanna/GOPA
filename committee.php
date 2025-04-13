9eddd8edb6cec9a7eaeb66a4c33f1632edd77990
Stephen Emery <emery.stephen@gmail.com>
2025-04-13 11:59:02 -0700

    2025 Board Updates

--- committee.php
+++ committee.php
@@ -20,42 +20,19 @@
       <div class="row">
         <div class="col-lg-1"></div>
         <div class="col-lg-10 body">
-          <h2>Board</h2>
-          <h3 class="committee">Board Members</h3>
+          <h2>Board Members</h2>
           <br>
-          <h4>Judy Han, Chair</h4>
+          <h4>Judy Han - Chair</h4>
           <p>Along with her husband, Sunny, Judy has owned and successfully operated Downtown Gresham's popular Sunny Han's Wok & Grill for twenty-nine years. She has also served as President of the Downtown Gresham Boosters and nineteen years ago founded the annual Gresham Art Walk, which over the years has seen a significant increase in highly visible artist participation and visitor attendance. Judy's contribution to the Gresham art scene has been considerable.</p>
           <br>
-          <h4>Rachel Emery, Treasurer</h4>
+          <h4>Rachel Emery - Treasurer</h4>
           <p>Rachel was born and raised in the Portland/Gresham area. Growing up she enjoyed learning about and doing various art forms from around the world. Rachel also volunteered for a few non-profits. She worked as a manager and served as a Board Member for <em>Chase Me Again</em>, a local nonprofit thrift store. She also designed brochures and flyers for GOPA in their early days. Currently she is a stay at home mom with two beautiful boys.</p>
           <br>
-          <h4>Charlotte Becker, Secretary</h4>
+          <h4>Charlotte Becker - Secretary</h4>
           <p>Charlotte was a member of the Gresham Art Walk committee for 11 years, implementing and overseeing recycling and waste management for the event. Until retiring at the end of December 2015, she served veterans and their families for 8 years as a licensed clinical social worker in Vancouver and East Multnomah County. Prior to the VA, Charlotte and her partner managed the Association of Oregon Recyclers and contracted with government agencies for public outreach materials. She was the “Voice of OPB TV” for 25 years and before retiring, the voice of Government Interactive Voice Response Systems in many US cities.</p>
-          <br>
-        </div> <!-- end of body -->
-      </div><!-- row div close -->
-      <div class="row">
-        <div class="col-lg-1"></div>
-        <div class="col-lg-10 body">
-          <h3 class="committee text-center">Board Members</h3>
-          <br>
-          <h4>Joan Albertson</h4>
-          <p>Joan is an active Gresham volunteer serving as a GPD Citizen Volunteer in Policing and a member of the Gresham Chamber of Commerce Government Affairs Committee. She was licensed by the Oregon Board of Accountancy as a Certified Public Accountant (now retired) in 1969 when there were less than 2000 women CPAs in the United States. After leaving public accounting, she was Controller for a business forms manufacturer for several years. Prior activities include the 2012 Gresham Charter Review Committee, Center for the Arts Foundation and serving as a local Campaign Treasurer.</p>
-          <br>
-          <h4>Rose Marie Noojin</h4>
-          <p>Rose Marie was previously employed as the Manager of Sunny Han's Wok & Grill in Historic Downtown Gresham. She serves as Secretary for the Gresham Historical Society. Some of her past positions in the Gresham community: served as Secretary for Gresham Outdoor Public Art (GOPA), Secretary for Art Walk Etc., Secretary for the Downtown Gresham Boosters and Secretary for the Mt. Hood Jazz Festival.</p>
-          <br>
-          <h4>David Baumann</h4>
-          <p>David is a private fine art collector and has been involved in the art industry through career and personal experiences for over thirty years. This experience has brought many long standing relationships with fine artists across the United States, and especially in the northwest region. He has been a Board Member of the Gresham Arts Commission, Mt. Hood Jazz Festival and the Gresham Historical Society as well as past President of the Troutdale Chamber of Commerce.</p>
           <br>
           <h4>Julie Frediani</h4>
           <p>Julie is a recently retired Gresham-Barlow teacher. She has been a supporter of the arts in her classroom and community for many years. Julie has helped with the Gresham Artwalk and contributed to the creation of some of the murals and sculptures in Gresham. She loves to sing and has always considered herself "right-brained". The arts in any form are important for self expression and bringing joy to others. Julie is proud to promote the arts in the Gresham community.</p>
-          <br>
-          <h4>Sandy Martin</h4>
-          <p>Sandy is retired after owning and operating a popular book store for 25 years in Historic Downtown Gresham, and she is still involved in community projects that she was instrumental in starting. Having lived in Gresham for 41 years before moving to Boring, she was involved in the Gresham Downtown Boosters, Historic Downtown Trick or Treat Candy Giveaway and My Mom's A Winner drawing contest, which are all still going strong today. She also sent books overseas to the soldiers during the Gulf War and set up a library at the Federal Prison in Sheridan. Sandy is a huge supporter of the Gresham Historical Society and the Gresham Outdoor Public Arts projects.</p>
-          <br>
-          <h4>Kathy Toynbee</h4>
-          <p>Kathy Toynbee has lived in Gresham for 36 years and has served the community as a volunteer in many ways, including as a seven-year member of the Mt. Hood Community College Foundation. Kathy's love for art began while working for bronze sculptor, Rip Caswell. During her years with Caswell, Kathy managed Caswell's public art commissions, from proposal to installation. Having met and developed relationships with many artists from the Pacific NW, Kathy's appreciation for art of all mediums has flourished. She believes art is meant to be shared with viewers from all walks of life and that art can help build community spirit and strength.</p>
           <br>
           <h4>Stephen Emery</h4>
           <p>Stephen and his two sons are members of the <em>Chickahominy Eastern Tribe</em>. He has worked with Gresham Outdoor Art for over six years doing a variety  of technical support  and building the website. He currently works as an Android and iOS developer focusing on augmented reality. Stephen and his wife Rachel have collected a number of art pieces around SE Asia and have started collecting Native art in the U.S. He is excited to bring Native and other cultural art to the Gresham area.</p>
@@ -63,8 +40,29 @@
           <h4>Joy Wilson</h4>
           <p>Joy has lived in the Gresham area for the past 20 years where she and her husband raise their 4 kids. Her children enjoy creating art through paint, charcoal and photography.  When not working as an Accountant, Joy enjoys living an active lifestyle running and hiking, and being a member of Eastwind Running and Endurance club. Joy is excited about joining GOPA, participating in a non-profit that cares so much about bringing art to our community.</p>
           <br>
-          <h4>Mike Hill      1945 - 2016 In Memoriam</h4>
-          <p>Mike was a watercolor artist extraordinaire. A member of the Gresham-Barlow School Board for nine years, Chairperson of Citizens for Schools, promoting school bond measures for over twenty years, past Board member and Artistic Advisor for the Gresham Art Walk from its inception, a jurist for the annual juried event, member of the National Watercolor Society, American Watercolor Society and signature member of the Northwest Watercolor Society. Mike's artwork is in many City Hall offices as well as a mural in the City Council Chambers.</p>
+          <h4>Kristina Etzel</h4>
+          <p>Kristina has been an elementary music teacher in Gresham and the surrounding communities for the last fifteen years. Along with her husband, Kristina served as a foster parent for eight years. During this time, visiting the outdoor art pieces around downtown Gresham became a favorite regular activity for all the kids. Dabbling in various fiber arts provides Kristina the chance to create unique and personalized gifts for family and friends. Kristina loves supporting opportunities for the kids of our community to engage in the arts.</p>
+        </div> <!-- end of body -->
+      </div><!-- row div close -->
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+          <h3>Emeritus Board Members</h3>
+          <br>
+          <h4>Joan Albertson</h4>
+          <p>Joan is an active Gresham volunteer serving as a GPD Citizen Volunteer in Policing and a member of the Gresham Chamber of Commerce Government Affairs Committee. She was licensed by the Oregon Board of Accountancy as a Certified Public Accountant (now retired) in 1969 when there were less than 2000 women CPAs in the United States. After leaving public accounting, she was Controller for a business forms manufacturer for several years. Prior activities include the 2012 Gresham Charter Review Committee, Center for the Arts Foundation and serving as a local Campaign Treasurer.</p>
+          <br>
+          <h4>Rose Marie Noojin</h4>
+          <p>Rose Marie was previously employed as the Manager of Sunny Han's Wok & Grill in Historic Downtown Gresham. She serves as Secretary for the Gresham Historical Society. Some of her past positions in the Gresham community: served as Secretary for Gresham Outdoor Public Art (GOPA), Secretary for Art Walk Etc., Secretary for the Downtown Gresham Boosters and Secretary for the Mt. Hood Jazz Festival.</p>
+        </div><!-- body div close -->
+      </div><!-- row div close -->
+      <div class="row">
+        <div class="col-lg-1"></div>
+        <div class="col-lg-10 body">
+          <h3>In Memoriam</h3>
+          <br>
+          <h4>Sandy Martin</h4>
+          <p>Sandy is retired after owning and operating a popular book store for 25 years in Historic Downtown Gresham, and she is still involved in community projects that she was instrumental in starting. Having lived in Gresham for 41 years before moving to Boring, she was involved in the Gresham Downtown Boosters, Historic Downtown Trick or Treat Candy Giveaway and My Mom's A Winner drawing contest, which are all still going strong today. She also sent books overseas to the soldiers during the Gulf War and set up a library at the Federal Prison in Sheridan. Sandy is a huge supporter of the Gresham Historical Society and the Gresham Outdoor Public Arts projects.</p>
         </div><!-- body div close -->
       </div><!-- row div close -->
     </div><!-- container div close -->
